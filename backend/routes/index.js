9a9da1938d27ffc41066fa63f93e6e063be08729
brandonvicc <bvicc93@gmail.com>
2022-02-03 12:43:28 -0600

    api router set up

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 
+const apiRouter = require('./api')
+
 const router = express.Router();
 
+router.use('/api', apiRouter)
+
 router.get("/hello/world", (req, res) => {
-  res.cookie("XSRF-TOKEN", res.csrfToken());
+  res.cookie("XSRF-TOKEN", req.csrfToken());
   res.send("Hello World!");
 });
 
