355ddb1f22552a8f3f010f28a60a16a5a9446455
brandonvicc <bvicc93@gmail.com>
2022-02-06 11:55:57 -0600

    started HomePage Component

---
+++ frontend/src/components/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+const HomePage = () => {
+  return (
+    <>
+      <h1>Find yourself outside.</h1>
+      <p>
+        Discover and book tent camping, RV parks, cabins, treehouses, and
+        glamping.
+      </p>
+    </>
+  );
+};
