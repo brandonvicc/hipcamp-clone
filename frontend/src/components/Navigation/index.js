24526ec80b867281618a24568f151bafe922fdef
brandonvicc <bvicc93@gmail.com>
2022-02-06 11:51:11 -0600

    navbar basic styling complete

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -13,21 +13,32 @@ function Navigation({ isLoaded }) {
   } else {
     sessionLinks = (
       <>
-        <NavLink to="/login">Log In</NavLink>
-        <NavLink to="/signup">Sign Up</NavLink>
+        <NavLink to="/login" className={"nav-links"}>
+          Log In
+        </NavLink>
+        <NavLink to="/signup" className={"nav-links"}>
+          Sign Up
+        </NavLink>
       </>
     );
   }
 
   return (
-    <ul>
-      <li>
-        <NavLink exact to="/">
-          Home
-        </NavLink>
-        {isLoaded && sessionLinks}
-      </li>
-    </ul>
+    <nav className="navbar">
+      <div className="logo-container">
+        <p className="logo">
+          iwanna<span>camp</span>
+        </p>
+      </div>
+      <ul className="nav-list">
+        <li className="nav-list-item">
+          <NavLink exact to="/" className={"nav-links"}>
+            Home
+          </NavLink>
+          {isLoaded && sessionLinks}
+        </li>
+      </ul>
+    </nav>
   );
 }
 
